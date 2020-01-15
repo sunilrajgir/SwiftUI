f1dc99f2776a6478768019dba2a6564ac20ca67f
Sunil-Kumar1 <Sunil.Kumar1@timesinternet.in>
2020-01-15 20:16:33 +0530

    added button action

--- MyFirstSwiftUI/MyFirstSwiftUI/BasicView.swift
+++ MyFirstSwiftUI/MyFirstSwiftUI/BasicView.swift
@@ -10,14 +10,35 @@ import SwiftUI
 import UIKit
 struct BasicView: View {
     @State var sliderValue = Double.random(in: 0..<1)
+    @State var isAlertShown = false
+    @State var isPresentationShown = false
     var body: some View {
         NavigationView {
-            VStack {
+            VStack(alignment: .center, spacing: 50) {
                 Rectangle().padding(20.0).frame(width: 200.0, height: 200.0).foregroundColor(Color(red: self.sliderValue, green: self.sliderValue, blue: self.sliderValue))
+                
                 Slider(value: self.$sliderValue).foregroundColor(.gray)
+                
+                Button(action: {
+                    self.isAlertShown = true
+                }) {
+                    Text("Button")
+                }.alert(isPresented: self.$isAlertShown) {
+                    Alert(title: Text("Thank You"), message: Text("We have learned button"))
+                }
+                
+                Button(action: {
+                    self.isPresentationShown = true
+                }) {
+                    Text("present view controller")
+                }.sheet(isPresented:self.$isPresentationShown) {
+                    NewsView(newsArray: testData)
+                }
+                
             }
-            .navigationBarTitle("My First App")
+            .navigationBarTitle("My First App", displayMode: .inline)
         }
+        .padding(20.0)
     }
 }
 
